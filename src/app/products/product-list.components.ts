06c37df379cd1847e558ab2c96beb821ee63f06e
showduhtung <shaun.tung@gmail.com>
2019-08-07 17:22:38 -0400

    starting on defining interfaces

--- src/app/products/product-list.components.ts
+++ src/app/products/product-list.components.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct} from './product'
 @Component({
   selector: 'pm-products', //product management root
   templateUrl: './product-list.component.html',
@@ -11,7 +12,7 @@ export class ProductListComponent {
   imageMargin: number = 2;
   showImage: boolean = false;
   listFilter: string = 'cart';
-  products : any[] = [{
+  products : IProduct[] = [{
     "productId": 2,
     "productName": "Garden Cart",
     "productCode": "GDN-0023",
